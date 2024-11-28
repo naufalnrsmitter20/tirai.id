7e76bc81e2e1a67415505e2ca5245fb6c44b372f
Devdit <pradita.cleine@gmail.com>
2024-11-28 15:38:29 +0700

    chore: add screen sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,7 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: { sm: "390px", md: "810px", lg: "1200px", xl: "1440px" },
       colors: {
         background: "#0D0D0D",
         foreground: "#FFFFFF",
