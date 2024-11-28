9cd214092c904411e2b83bd3d701d235589a4baa
Ahsan <ahsanaz461@gmail.com>
2024-11-28 14:29:43 +0700

    feat: Add responsive testimony section in landing page

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -3,14 +3,15 @@
 import { PageContainer } from "@/components/layout/PageContainer";
 import { SectionContainer } from "@/components/layout/SectionContainer";
 import { buttonVariants } from "@/components/ui/button";
-import { Body3, Display, H1, H2, H3, H5 } from "@/components/ui/text";
+import { Body1, Body3, Display, H1, H2, H3, H5 } from "@/components/ui/text";
 import { SectionTitle } from "@/components/widgets/SectionTitle";
 import { cn, formatRupiah } from "@/lib/utils";
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Quote } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { ReactNode, useEffect, useState } from "react";
 import { Navbar } from "./components/navbar";
+import { COLORS } from "@/constants/color";
 
 interface ProductType {
   icon: ReactNode;
@@ -29,6 +30,14 @@ interface Product {
   name: string;
   description: string;
   price: number;
+}
+
+interface Testimony {
+  testimony: string;
+  customer: {
+    name: string;
+    title: string;
+  };
 }
 
 const CAROUSEL_IMAGES: string[] = [
@@ -246,6 +255,24 @@ const FABRIC_HIGHLIGHTS: Motto[] = [
   },
 ];
 
+const TESTIMONIES: Testimony[] = [
+  {
+    testimony:
+      "Saya sangat puas dengan kualitas tirai yang saya beli! Proses pemesanan sangat mudah dan hasilnya luar biasa.",
+    customer: { name: "Rina Agustina", title: "Ibu Rumah Tangga" },
+  },
+  {
+    testimony:
+      "Saya sangat puas dengan kualitas tirai yang saya beli! Proses pemesanan sangat mudah dan hasilnya luar biasa.",
+    customer: { name: "Rina Agustina", title: "Ibu Rumah Tangga" },
+  },
+  {
+    testimony:
+      "Saya sangat puas dengan kualitas tirai yang saya beli! Proses pemesanan sangat mudah dan hasilnya luar biasa.",
+    customer: { name: "Rina Agustina", title: "Ibu Rumah Tangga" },
+  },
+];
+
 // TODO: Remove these dummy products and replace it with an actual list of products using server action
 const PRODUCTS: Product[] = [
   {
@@ -289,6 +316,7 @@ const PRODUCTS: Product[] = [
 export default function Home() {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [currentFabricHighlight, setCurrentFabricHighlight] = useState(0);
+  const [currentTestimony, setCurrentTestimony] = useState(0);
 
   // Change current slide and fabric every 5 seconds
   useEffect(() => {
@@ -590,6 +618,67 @@ export default function Home() {
               height={475}
               className="mb-14 h-auto w-full max-w-[381px] object-cover md:mb-24 lg:mb-0"
             />
+          </div>
+        </SectionContainer>
+        <SectionContainer id="testimony">
+          <div className="flex w-full flex-col items-start justify-between gap-y-24 md:flex-row">
+            <div className="w-full lg:max-w-[45%]">
+              <SectionTitle>Testimoni</SectionTitle>
+              <H1 className="mb-[1.375rem] text-black">
+                Bagaimana Tirai.id Dipercaya Banyak Pelanggan
+              </H1>
+              <Body3 className="mb-12 text-neutral-500">
+                Pelajari lebih lanjut bagaimana produk kami memberikan kepuasan
+                kepada pelanggan kami. Testimoni ini adalah bukti kepercayaan
+                yang telah kami bangun.
+              </Body3>
+              <div className="flex w-fit items-center gap-x-2">
+                {Array.from({ length: TESTIMONIES.length }).map((_, index) => (
+                  <button
+                    key={index}
+                    onClick={() => setCurrentTestimony(index)}
+                  >
+                    <div
+                      className={cn(
+                        "rounded-full bg-primary-900 transition-all duration-300",
+                        currentTestimony === index
+                          ? "size-[0.625rem] bg-opacity-100"
+                          : "size-2 bg-opacity-[28%]",
+                      )}
+                    ></div>
+                  </button>
+                ))}
+              </div>
+            </div>
+            <div className="w-full overflow-x-hidden lg:max-w-[45%]">
+              <div
+                className="flex h-full w-full transition-transform duration-300"
+                style={{
+                  transform: `translateX(-${currentTestimony * 100}%)`,
+                }}
+              >
+                {TESTIMONIES.map((testimony, index) => (
+                  <div key={index} className="h-full w-full flex-shrink-0">
+                    <div className="mb-6">
+                      <Quote
+                        size={24}
+                        fill={COLORS.primary[900]}
+                        color={COLORS.primary[900]}
+                      />
+                    </div>
+                    <Body1 className="mb-12 text-black">
+                      {testimony.testimony}
+                    </Body1>
+                    <div className="flex flex-col gap-y-0.5">
+                      <H5 className="text-black">{testimony.customer.name}</H5>
+                      <Body3 className="text-neutral-500">
+                        {testimony.customer.title}
+                      </Body3>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </SectionContainer>
       </PageContainer>
