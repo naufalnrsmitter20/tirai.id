724f5d9543118f758afcaafe8a369631647ef57c
Ahsan <ahsanaz461@gmail.com>
2024-11-29 16:16:43 +0700

    feat: Update sitemap config to contain all articles

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,18 +3,28 @@ import { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const articles = await prisma.article.findMany({
-    select: { slug: true, updated_at: true },
+    select: { slug: true, updated_at: true, tags: true },
   });
+
+  const tags = articles.map(({ tags }) => tags).flat();
 
   return [
     { url: `${process.env.URL}/`, lastModified: new Date().toISOString() },
+    {
+      url: `${process.env.URL}/about`,
+      lastModified: new Date().toISOString(),
+    },
     {
       url: `${process.env.URL}/article`,
       lastModified: new Date().toISOString(),
     },
     ...articles.map(({ slug, updated_at }) => ({
-      url: `${process.env.APP_URL}/article/${slug}`,
+      url: `${process.env.APP_URL}/article/view/${slug}`,
       lastModified: updated_at.toISOString(),
+    })),
+    ...tags.map((tag) => ({
+      url: `${process.env.APP_URL}/article/tags/${tag}`,
+      lastModified: new Date(),
     })),
   ];
 }
