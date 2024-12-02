3bbb76ef5e9755ba8c640697e460d1bdc4a6f98d
Ahsan <ahsanaz461@gmail.com>
2024-12-02 14:16:23 +0700

    fix: Change clients layouting to grid in about us page

--- src/app/(use-navbar)/about/components/Client.tsx
+++ src/app/(use-navbar)/about/components/Client.tsx
@@ -1,11 +1,7 @@
 import { SectionContainer } from "@/components/layout/SectionContainer";
-import { buttonVariants } from "@/components/ui/button";
 import { Body3, H2, H5 } from "@/components/ui/text";
 import { SectionTitle } from "@/components/widgets/SectionTitle";
-import { formatRupiah } from "@/lib/utils";
-import { ArrowRight } from "lucide-react";
 import Image from "next/image";
-import Link from "next/link";
 import { FC } from "react";
 
 interface Clients {
@@ -49,60 +45,37 @@ const CLIENTS: Clients[] = [
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ligula",
   },
 ];
-export const Client: FC<{ clients?: Clients[] }> = (
-  { clients } = { clients: CLIENTS },
-) => {
+export const Client: FC = () => {
   return (
     <SectionContainer>
       <div className="flex flex-col">
         <SectionTitle>Klien</SectionTitle>
-        <div className="flex w-full flex-col justify-between space-y-[1.25rem] lg:flex-row lg:space-y-0">
+        <div className="mb-12 flex w-full flex-col justify-between space-y-[1.25rem] lg:flex-row lg:space-y-0">
           <H2 className="text-black">
             Membantu Mewujudkan Ruang Impian Banyak Klien
           </H2>
-          <Link
-            href={"#"}
-            className={buttonVariants({
-              variant: "default",
-              className: "w-[212px] lg:w-fit",
-            })}
-          >
-            Lihat lebih banyak <ArrowRight />
-          </Link>
         </div>
-        <ul className="no-scrollbar flex w-full snap-x snap-mandatory flex-col items-end justify-between gap-6 overflow-x-auto pb-10 sm:flex-row sm:items-start">
-          {clients && clients.length > 0 ? (
-            clients.map((clients) => (
-              <li
-                key={clients.id}
-                className="flex w-[85%] flex-none snap-start flex-col items-center md:w-[40%] lg:w-[23%]"
-              >
-                <div className="w-full">
-                  <Image
-                    src={clients.photo}
-                    alt={clients.name}
-                    width={273}
-                    height={304}
-                    className="mb-11 min-h-[16rem] w-full rounded-[1.25rem] object-cover"
-                    unoptimized
-                  />
-                  <div className="flex flex-col items-start text-black">
-                    {/* TODO: Change the href into actual clients detail */}
-                    <Link href={"#"}>
-                      <H5 className="mb-3">{clients.name}</H5>
-                    </Link>
-                    <Body3 className="mb-5 text-neutral-500">
-                      {clients.description}
-                    </Body3>
-                  </div>
+        <ul className="grid w-full grid-cols-1 items-start justify-between gap-6 pb-10 md:grid-cols-3 lg:grid-cols-4">
+          {CLIENTS.map((client) => (
+            <li key={client.id} className="flex flex-col items-center">
+              <div className="w-full">
+                <Image
+                  src={client.photo}
+                  alt={client.name}
+                  width={273}
+                  height={304}
+                  className="mb-11 h-40 w-full rounded-2xl object-cover"
+                  unoptimized
+                />
+                <div className="flex flex-col items-start">
+                  <H5 className="mb-3 text-black">{client.name}</H5>
+                  <Body3 className="mb-5 text-neutral-500">
+                    {client.description}
+                  </Body3>
                 </div>
-              </li>
-            ))
-          ) : (
-            <Body3 className="text-neutral-500">
-              Belum ada testimoni dari klien...
-            </Body3>
-          )}
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </SectionContainer>
