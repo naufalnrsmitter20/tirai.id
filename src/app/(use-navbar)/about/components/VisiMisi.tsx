364e67900ffbe993f0192bc056f4309247cba0fc
Ahsan <ahsanaz461@gmail.com>
2024-12-02 13:21:42 +0700

    fix: Restructure spacings in about us page

--- src/app/(use-navbar)/about/components/VisiMisi.tsx
+++ src/app/(use-navbar)/about/components/VisiMisi.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
 import { SectionContainer } from "@/components/layout/SectionContainer";
-import { Body3, H2 } from "@/components/ui/text";
+import { Body3, H2, H3 } from "@/components/ui/text";
 import { SectionTitle } from "@/components/widget/SectionTitle";
+import Image from "next/image";
 
 export default function VisiMisi() {
   return (
     <SectionContainer>
       <SectionTitle>Visi & Misi</SectionTitle>
       <H2 className="mb-[3.875rem] w-full text-black lg:max-w-[50%]">
-        Komitmen Kami untuk Memberikan Layanan Tirai Terbaik
+        Komitmen Kami untuk Membuat Tirai dengan Kualitas Terbaik
       </H2>
-      <div className="mb-[3.875rem] flex flex-col justify-between lg:flex-row-reverse">
+      <div className="mb-[3.875rem] flex flex-col justify-between gap-y-12 lg:flex-row-reverse lg:gap-0">
         <Image
           src={"/assets/vision.png"}
           alt="Asset Visi"
@@ -18,17 +18,17 @@ export default function VisiMisi() {
           height={160}
           className="pointer-events-none mb-[1.25rem] h-auto w-full object-cover lg:mb-0 lg:w-[32%]"
         />
-        <div className="flex w-full flex-col justify-between lg:max-w-[55%]">
-          <H2 className="mb-[3.75rem] text-black lg:mb-0">Visi</H2>
-          <Body3 className="text-black">
+        <div className="flex w-full flex-col lg:max-w-[55%]">
+          <H3 className="mb-6 text-black lg:mb-8">Visi Kami</H3>
+          <Body3 className="text-neutral-500">
             Menjadi penyedia tirai terpercaya di Indonesia yang mengutamakan
             kualitas, inovasi, dan kepuasan pelanggan untuk menciptakan ruang
             yang indah dan nyaman. Kami bercita-cita untuk membantu menciptakan
-            ruangan yang lebih nyaman dan estetis bagi setiap pelanggan
+            ruangan yang lebih nyaman dan estetis bagi setiap pelanggan.
           </Body3>
         </div>
       </div>
-      <div className="flex flex-col justify-between lg:flex-row-reverse">
+      <div className="flex flex-col justify-between gap-y-12 lg:flex-row-reverse lg:gap-0">
         <Image
           src={"/assets/mission.png"}
           alt="Asset Misi"
@@ -36,12 +36,12 @@ export default function VisiMisi() {
           height={160}
           className="pointer-events-none mb-[1.25rem] h-auto w-full object-cover lg:mb-0 lg:w-[32%]"
         />
-        <div className="flex w-full flex-col justify-between lg:max-w-[55%]">
-          <H2 className="mb-[3.75rem] text-black lg:mb-0">Misi</H2>
-          <Body3 className="text-black">
+        <div className="flex w-full flex-col lg:max-w-[55%]">
+          <H3 className="mb-6 text-black lg:mb-8">Misi Kami</H3>
+          <Body3 className="text-neutral-500">
             Kami berkomitmen untuk menyediakan tirai berkualitas tinggi dengan
             desain estetis dan fungsional, memberikan layanan pemesanan yang
-            cepat dan tepat, serta selalu mengutamakan kepuasan pelanggan
+            cepat dan tepat, serta selalu mengutamakan kepuasan pelanggan.
           </Body3>
         </div>
       </div>
