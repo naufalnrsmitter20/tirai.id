8fcfccf90264002894a369d0d0fa2d042c3be8cd
Ahsan <ahsanaz461@gmail.com>
2024-12-03 10:23:19 +0700

    feat: Add navigation to main dashboard

--- src/app/admin/components/Sidebar.tsx
+++ src/app/admin/components/Sidebar.tsx
@@ -1,51 +1,55 @@
-import { Newspaper, User2, UserCog } from "lucide-react";
+import { CircleGauge, Home, Newspaper, User2 } from "lucide-react";
 import { SidebarMainContent } from "./SidebarMainContent";
 
 import {
   Sidebar,
   SidebarContent,
   SidebarHeader,
-  SidebarMenuItem,
   SidebarMenu,
   SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar";
 
-const data = {
-  navMain: [
-    {
-      title: "User",
-      url: "/admin/user",
-      icon: User2,
-      isActive: true,
-      items: [
-        {
-          title: "All Users",
-          url: "/admin/user",
-        },
-        {
-          title: "Add User",
-          url: "/admin/user/add",
-        },
-      ],
-    },
-    {
-      title: "Articles",
-      url: "/admin/article",
-      icon: Newspaper,
-      isActive: true,
-      items: [
-        {
-          title: "All Articles",
-          url: "/admin/article",
-        },
-        {
-          title: "Add Article",
-          url: "/admin/article/add",
-        },
-      ],
-    },
-  ],
-};
+const SIDEBAR_ITEMS = [
+  {
+    title: "Dashboard",
+    url: "/admin",
+    icon: Home,
+    isActive: true,
+  },
+  {
+    title: "User",
+    url: "/admin/user",
+    icon: User2,
+    isActive: true,
+    children: [
+      {
+        title: "All Users",
+        url: "/admin/user",
+      },
+      {
+        title: "Add User",
+        url: "/admin/user/add",
+      },
+    ],
+  },
+  {
+    title: "Articles",
+    url: "/admin/article",
+    icon: Newspaper,
+    isActive: true,
+    children: [
+      {
+        title: "All Articles",
+        url: "/admin/article",
+      },
+      {
+        title: "Add Article",
+        url: "/admin/article/add",
+      },
+    ],
+  },
+];
 
 export function AppSidebar() {
   return (
@@ -58,7 +62,7 @@ export function AppSidebar() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                <UserCog className="size-4" />
+                <CircleGauge className="size-4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">
@@ -70,7 +74,7 @@ export function AppSidebar() {
         </SidebarMenu>
       </SidebarHeader>
       <SidebarContent>
-        <SidebarMainContent items={data.navMain} />
+        <SidebarMainContent items={SIDEBAR_ITEMS} />
       </SidebarContent>
     </Sidebar>
   );
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,13 @@ export default withAuth(function middleware(_) {}, {
   callbacks: {
     authorized: ({ req, token }) => {
       const pathname = req.nextUrl.pathname;
-      if (pathname.startsWith("/auth") && token) {
+      if (
+        (pathname.startsWith("/auth") && token) ||
+        (pathname.startsWith("/admin") && !token) ||
+        (pathname.startsWith("/admin") && token?.role === "CUSTOMER")
+      ) {
         return false;
       }
-
-      if (pathname.startsWith("/admin") && token?.role === "CUSTOMER")
-        return false;
 
       return true;
     },
