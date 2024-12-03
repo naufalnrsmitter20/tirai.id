376d1edd3b36ed220fbbfe50771b6bc34d32f93e
Ahsan <ahsanaz461@gmail.com>
2024-12-03 10:32:22 +0700

    feat: Add toast to indicate image upload progression in content editor

--- src/components/editor/image-upload.ts
+++ src/components/editor/image-upload.ts
@@ -1,13 +1,17 @@
 "use client";
 
-import { createImageUpload } from "novel/plugins";
 import { uploadImageCloudinary } from "@/actions/fileUploader";
+import { createImageUpload } from "novel/plugins";
+import { toast } from "sonner";
 
 const onUpload = async (file: File) => {
+  const loadingToast = toast.loading("Mengupload gambar...");
+
   const cloudinaryResponse = await uploadImageCloudinary(
     await file.arrayBuffer(),
   );
   if (cloudinaryResponse.error) {
+    toast.error("Gagal mengupload gambar", { id: loadingToast });
     throw new Error(cloudinaryResponse.error.message);
   }
 
@@ -15,6 +19,7 @@ const onUpload = async (file: File) => {
     // preload the image
     const image = new Image();
     if (cloudinaryResponse.data?.url) {
+      toast.success("Sukses mengupload gambar", { id: loadingToast });
       image.src = cloudinaryResponse.data.url;
     }
     image.onload = () => {
@@ -28,7 +33,8 @@ export const uploadFn = createImageUpload({
   validateFn: (file) => {
     if (!file.type.includes("image/")) {
       return false;
-    } else if (file.size / 1024 / 1024 > 20) {
+    } else if (file.size / 1024 / 1024 > 10) {
+      // Check if the file size is bigger than 10mb
       return false;
     }
     return true;
