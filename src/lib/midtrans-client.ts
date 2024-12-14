c585715f07c505c2869b2277255a9cab65e6f1fe
Ben <me@benspace.xyz>
2024-12-14 20:17:27 +0700

    feat: Add validation Midtrans environment variables

--- src/lib/midtrans-client.ts
+++ src/lib/midtrans-client.ts
@@ -10,12 +10,18 @@ if (!process.env.MIDTRANS_SERVER_KEY) {
 
 const MIDTRANS_CLIENT_KEY = process.env.NEXT_PUBLIC_MIDTRANS_CLIENT_KEY;
 const MIDTRANS_SERVER_KEY = process.env.MIDTRANS_SERVER_KEY;
-const isProduction = process.env.NODE_ENV === "production";
+
+const isSandbox = MIDTRANS_CLIENT_KEY.startsWith("SB-");
 
 const snapClient = new Snap({
-  isProduction,
+  isProduction: !isSandbox,
   serverKey: MIDTRANS_SERVER_KEY,
   clientKey: MIDTRANS_CLIENT_KEY,
 });
 
+const MIDTRANS_SNAP_URL = `https://app.${
+  isSandbox ? "sandbox." : ""
+}midtrans.com/snap/snap.js`;
+
+export { MIDTRANS_CLIENT_KEY, MIDTRANS_SNAP_URL };
 export default snapClient;
