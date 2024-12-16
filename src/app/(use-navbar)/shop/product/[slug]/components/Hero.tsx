2fd592a87555d7db5ba8b86685b4d9b778e4f1c8
Ahsan <ahsanaz461@gmail.com>
2024-12-16 21:05:50 +0700

    feat: Add responsive hero section in product detail page
    Co-authored-by: Cleine Pradita <Devdit@users.noreply.github.com>

---
+++ src/app/(use-navbar)/shop/product/[slug]/components/Hero.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { SectionContainer } from "@/components/layout/SectionContainer";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Body3, H3, H5 } from "@/components/ui/text";
+import { cn } from "@/lib/utils";
+import { Prisma } from "@prisma/client";
+import { ShoppingCart } from "lucide-react";
+import Image from "next/image";
+import { FC, useEffect, useMemo, useState } from "react";
+
+type Product = Prisma.ProductGetPayload<{ include: { variants: true } }>;
+
+export const Hero: FC<{ product: Product }> = ({ product }) => {
+  const [selectedVariant, setSelectedVariant] = useState(
+    product.variants[0] || null,
+  );
+  const [selectedPhoto, setSelectedPhoto] = useState(product.photos[0]);
+  const [selectedStock, setSelectedStock] = useState(1);
+
+  const maxStock = useMemo(
+    () => product.stock || selectedVariant.stock,
+    [selectedVariant],
+  );
+
+  useEffect(() => {
+    if (selectedStock > maxStock) {
+      setSelectedStock(maxStock);
+    }
+  }, [maxStock]);
+
+  return (
+    <SectionContainer id="hero">
+      <div className="flex flex-col justify-between md:flex-row">
+        <div className="flex w-full flex-col-reverse gap-6 md:max-w-[56%] md:flex-row">
+          <div className="mb-12 flex w-full flex-row gap-5 md:mb-0 md:w-24 md:flex-col">
+            {product.photos.map((photo) => (
+              <button
+                key={photo}
+                className={cn(
+                  "size-fit",
+                  photo === selectedPhoto
+                    ? "rounded-2xl border-2 border-primary-900"
+                    : "",
+                )}
+                onClick={() => setSelectedPhoto(photo)}
+              >
+                <Image
+                  src={photo}
+                  alt="Small Product Image"
+                  unoptimized
+                  className="aspect-square size-24 rounded-2xl"
+                  height={98}
+                  width={98}
+                />
+              </button>
+            ))}
+          </div>
+          <Image
+            src={selectedPhoto}
+            alt="Large Product Image"
+            unoptimized
+            className="h-[372px] w-full max-w-[522px] rounded-[20px]"
+            height={372}
+            width={522}
+          />
+        </div>
+        <div className="flex w-full flex-col md:max-w-[38%]">
+          <H3 className="mb-[.75rem] text-black">{product.name}</H3>
+          <Body3 className="mb-[3.5rem] text-neutral-500">
+            {product.description}
+          </Body3>
+          {product.variants.length > 0 && (
+            <div className="flex flex-col">
+              <H5 className="mb-4 text-black">Pilihan Varian</H5>
+              <div className="grid grid-cols-3 gap-4 md:grid-cols-8">
+                {product.variants.map((variant) => (
+                  <Button
+                    key={variant.id}
+                    variant={"default"}
+                    className={cn(
+                      "rounded-xl duration-0",
+                      variant.id === selectedVariant.id
+                        ? ""
+                        : "border border-primary-900 bg-white text-primary-900 hover:text-white",
+                    )}
+                    onClick={() => setSelectedVariant(variant)}
+                  >
+                    {variant.name}
+                  </Button>
+                ))}
+              </div>
+            </div>
+          )}
+          <div className="mt-8 flex w-full flex-col items-center gap-2">
+            <div className="flex w-full flex-col">
+              <Label className="mb-2 text-black">Kuantitas</Label>
+              <Select
+                onValueChange={(value) => {
+                  setSelectedStock(Number(value));
+                }}
+                value={selectedStock.toString()}
+              >
+                <SelectTrigger className="w-full">
+                  <SelectValue>{selectedStock}</SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  {Array.from(
+                    { length: maxStock > 30 ? 29 : maxStock },
+                    (_, index) => index + 1,
+                  ).map((sortOption) => (
+                    <SelectItem key={sortOption} value={sortOption.toString()}>
+                      {sortOption}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            </div>
+            <Button variant={"default"} className="w-full">
+              Masukkan Keranjang <ShoppingCart />
+            </Button>
+          </div>
+        </div>
+      </div>
+    </SectionContainer>
+  );
+};
