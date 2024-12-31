88f812499d7077760975c4de9041b64e34719b50
Ben <me@benspace.xyz>
2024-12-31 17:38:21 +0700

    fix: SearchBar component to display conversations in descending order of creation

--- src/app/admin/chat/components/SearchBar.tsx
+++ src/app/admin/chat/components/SearchBar.tsx
@@ -26,30 +26,32 @@ export const SearchBar = ({
         <Input placeholder="Cari pesan..." onChange={handleFiltering} />
       </div>
       <div className="mt-[12px] flex h-full max-h-[100%] w-full flex-col gap-1 overflow-y-scroll sm:mt-[70px]">
-        {filteredConvo.map((i) => (
-          <figure
-            key={i.id}
-            onClick={() => {
-              handleConversationClick(i.customer_id);
-            }}
-            className={cn(
-              "relative flex aspect-[6/1] w-full items-center gap-2 rounded-lg px-3 py-1 transition-all duration-300 hover:bg-neutral-100 sm:grid sm:aspect-[5/1] sm:grid-cols-[15%_85%]",
-              i.customer_id === activeChat ? "bg-neutral-100" : "bg-white",
-            )}
-          >
-            <div className="inline-flex !aspect-square h-[30px] items-center justify-center rounded-full bg-purple-400 text-white">
-              {recipients
-                .find((j) => j.id === i.customer_id)
-                ?.name[0].toUpperCase()}
-            </div>
-            <div className="text-black">
-              {recipients.find((j) => j.id === i.customer_id)?.name}
-            </div>
-            {i.sender_id !== session?.user?.id && !i.is_read && (
-              <div className="absolute right-0 top-1/2 me-6 aspect-square w-2 -translate-y-1/2 rounded-full bg-blue-700" />
-            )}
-          </figure>
-        ))}
+        {filteredConvo
+          .toSorted((a, b) => (a.created_at > b.created_at ? -1 : 1))
+          .map((i) => (
+            <button
+              key={i.id}
+              onClick={() => {
+                handleConversationClick(i.customer_id);
+              }}
+              className={cn(
+                "relative flex aspect-[6/1] w-full items-center gap-2 rounded-lg px-3 py-1 transition-all duration-300 hover:bg-neutral-100 sm:grid sm:aspect-[5/1] sm:grid-cols-[15%_85%]",
+                i.customer_id === activeChat ? "bg-neutral-100" : "bg-white",
+              )}
+            >
+              <div className="inline-flex !aspect-square h-[30px] items-center justify-center rounded-full bg-purple-400 text-white">
+                {recipients
+                  .find((j) => j.id === i.customer_id)
+                  ?.name[0].toUpperCase()}
+              </div>
+              <div className="text-black text-start">
+                {recipients.find((j) => j.id === i.customer_id)?.name}
+              </div>
+              {i.sender_id !== session?.user?.id && !i.is_read && (
+                <div className="absolute right-0 top-1/2 me-6 aspect-square w-2 -translate-y-1/2 rounded-full bg-blue-700" />
+              )}
+            </button>
+          ))}
       </div>
     </div>
   );
